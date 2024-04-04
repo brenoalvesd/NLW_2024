a5426f985352dc42e599db1d24bb8d0a5df5eef5
Breno Alves <breno7572@gmail.com>
2024-04-04 00:23:49 -0300

    Implementing get_event Route

--- src/data/event_handler.py
+++ src/data/event_handler.py
@@ -16,3 +16,24 @@ class EventHandler:
             body={ "eventID": body["uuid"] },
             status_code=200
         )
+    
+    def find_by_id(self, http_request: HTTPRequest) -> HTTPResponse:
+        event_id = http_request.param["event_id"]
+        event = self.__events_repository.get_event_by_id(event_id)
+        if not event: raise Exception("Event not found")
+
+        event_attendees_count = self.__events_repository.count_event_attendees(event_id)
+        
+        return HTTPResponse(
+            body={
+                "event": {
+                    "id": event.id,
+                    "title": event.title,
+                    "details": event.details,
+                    "slug": event.slug,
+                    "maximum_attendees": event.maximum_attendees,
+                    "attendees_amount": event_attendees_count["attendees_amount"] 
+                }
+            },
+            status_code=200
+                )
